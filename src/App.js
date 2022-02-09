b8bac458e5becb87a9884a42f10c82ba0b340031
caio1902araujo <caio2002araujo@gmail.com>
2022-02-08 21:25:32 -0300

    cleaning style and setting routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import News from './pages/news';
+import CategoryNews from './pages/categoryNews';
+import Search from './pages/search';
 
 function App() {
   return (
-    <div className="App">
-      Teste
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/:titleNews" element={<News/>}/>
+          <Route path="/categoria/:category" element={<CategoryNews/>}/>
+          <Route path="/pesquisar" element={<Search/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
