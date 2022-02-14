6839dc8749e21c8ea3f36e086b7af273afcd43fc
caio1902araujo <caio2002araujo@gmail.com>
2022-02-14 19:48:42 -0300

    Implementing logo component

---
+++ src/components/logo/logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './logo.module.css';
+
+const Logo = () => {
+  return <span className={styles.logo}>cross<span className={styles.green}>life</span></span>;
+};
+
+export default Logo;
