d03841382c31aa7a78f9f66a5a10b7fa989b789c
caio1902araujo <caio2002araujo@gmail.com>
2022-02-17 23:01:40 -0300

    Adding routes to the pagination and header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import './App.css';
+import './generalStyles/reset.module.css';
+import './generalStyles/colors.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import News from './pages/news';
 import CategoryNews from './pages/categoryNews';
 import Search from './pages/search';
+import Header from './components/header/header';
 
 function App() {
+  const [theme, setTheme] = React.useState('light');
+
   return (
-    <div>
+    <div data-theme={theme}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/:titleNews" element={<News/>}/>
-          <Route path="/categoria/:category" element={<CategoryNews/>}/>
-          <Route path="/pesquisar" element={<Search/>}/>
-        </Routes>
+        <Header setTheme={setTheme}/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/:titleNews" element={<News/>}/>
+            <Route path="/categoria/:category" element={<CategoryNews/>}/>
+            <Route path="/pesquisar" element={<Search/>}/>
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
