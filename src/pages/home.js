84a2724d454f1a06bec07e5ad4c5658e703c1698
caio1902araujo <caio2002araujo@gmail.com>
2022-03-12 12:29:40 -0300

    Adding intro and section component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Intro from '../components/intro/intro';
+import Section from '../components/section/section';
 
 const Home = () => {
   return (
     <div>
-      home
+      <Intro title='crosslife news' 
+        description='Se increva para fazer parte da comunidade crosslife e acelere em direção aos seus objetivos com notícias e dicas'
+        textButton='inscreva-se'
+      />
+
+      <Section title="mais lidas" styleSection="sectionPrimary" typeGridNews="mostReadNews"/>
+
+      <Section title="notícias" styleSection="sectionSecondary" typeGridNews="allNews"/>
     </div>
   );
 };
