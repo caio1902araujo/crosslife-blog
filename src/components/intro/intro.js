1777dd26908442a5d4b79b4d1279a659d0e2b8ac
caio1902araujo <caio2002araujo@gmail.com>
2022-03-14 12:22:09 -0300

    Adding different types of styles to the intro

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -3,12 +3,27 @@ import styles from './intro.module.css';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Intro = ({title, description, textButton}) => {
+const typesIntroStyle = {
+  primary: {
+    introStyle: styles.introPrimary,
+    titleStyle: styles.titleWhite,
+    descriptionStyle: styles.descriptionWhite,
+  },
+  secondary: {
+    introStyle: styles.introSecondary,
+    titleStyle: styles.title,
+    descriptionStyle: styles.description,
+  }
+}
+
+const Intro = ({title, description, textButton, typeIntroStyle}) => {
+  const {introStyle, titleStyle, descriptionStyle} = typesIntroStyle[typeIntroStyle];
+
   return (
-    <section className={styles.intro}>
+    <section className={introStyle}>
       <div className={styles.wrapper}>
-        <h1 className={styles.title}>{title}</h1>
-        <p className={styles.description}>{description}</p>
+        <h1 className={titleStyle}>{title}</h1>
+        <p className={descriptionStyle}>{description}</p>
         {textButton && <Link to="#" className={styles.callToAction}>{textButton}</Link>}
       </div>
     </section>
@@ -19,6 +34,7 @@ Intro.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   textButton: PropTypes.string,
+  typeIntroStyle: PropTypes.string,
 };
 
 export default Intro
