bf1ce536f84b593f912943dca43c40ff52333e8c
caio1902araujo <caio2002araujo@gmail.com>
2022-03-22 19:41:47 -0300

    Creating component for checking link types

---
+++ src/components/a/a.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const changeActive = (isActive) => (isActive ? {color: "#6be067"}:{});
+
+const A = ({children, url, className, isExternalLink=false, isNavLink=false, ...props}) => {
+  if(isExternalLink){
+    return <a href={url} className={className} target="_blank" rel="noopener noreferrer" {...props}> {children} </a>
+  }
+
+  if(isNavLink){
+    return <NavLink to={url} className={className} style={({isActive}) => changeActive(isActive)} {...props}> {children} </NavLink>
+  }
+
+  return <Link to={url} className={className} {...props}> {children} </Link>
+}
+
+A.defaultProps = {
+  isExternalLink: false,
+  isNavLink: false,
+};
+
+A.propTypes = {
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  isExternalLink: PropTypes.bool,
+  isNavLink: PropTypes.bool,
+};
+
+export default A;
